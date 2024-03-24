d3e5297e5103302cb1a77d5cdc31c6672dc2b2c3
Rawand J. Rasool <73317192+RawanD201@users.noreply.github.com>
2024-03-25 01:01:59 +0300

    Update list

--- main.go
+++ main.go
@@ -8,39 +8,39 @@ import (
 	"time"
 )
 
-type Film struct {
+type Todo struct {
 	Title string
 }
 
 func main() {
 	fmt.Println("Go app...")
 
-	// handler function #1 - returns the index.html template, with film data
+	// handler function #1 - returns the index.html template, with Todo data
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		films := map[string][]Film{
-			"Films": {
+		todo := map[string][]Todo{
+			"Todos": {
 				{Title: "Check email and respond to urgent messages"},
 				{Title: "Review calendar and schedule for the day"},
 				{Title: "Complete a short but important task to get momentum going"},
 			},
 		}
-		tmpl.Execute(w, films)
+		tmpl.Execute(w, todo)
 	}
 
-	// handler function #2 - returns the template block with the newly added film, as an HTMX response
+	// handler function #2 - returns the template block with the newly added todos, as an HTMX response
 	h2 := func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Second)
 		title := r.PostFormValue("title")
-		// htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title)
+		// htmlStr := fmt.Sprintf("<li>%s </li>", title)
 		// tmpl, _ := template.New("t").Parse(htmlStr)
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title})
+		tmpl.ExecuteTemplate(w, "=todo-list", Todo{Title: title})
 	}
 
 	// define handlers
 	http.HandleFunc("/", h1)
-	http.HandleFunc("/add-film/", h2)
+	http.HandleFunc("/add-todo/", h2)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 
